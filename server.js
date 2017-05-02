02767d4da30c1da8260e9680273293a51c0abe58
Alex Harley <alexbruceharley@gmail.com>
2017-05-02 12:15:29 +0200

    fix: req.body.authData contains the correct data

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const app = express()
 app.use(bodyParser.json())
 
 app.post('/login', (req, res) => {
-	if (req.body.email === 'webhook@user.com' &&
-		req.body.password === 'password') {
+	if (req.body.authData.email === 'webhook@user.com' &&
+		req.body.authData.password === 'password') {
 		res.json({
 			userId: Date.now(),
 			clientData: { favouriteColour: 'blue' },
